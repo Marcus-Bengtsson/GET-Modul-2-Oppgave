50b813f5facbcea28f6f3bb2438c78ad14809538
Marcus-Bengtsson <92784457+Marcus-Bengtsson@users.noreply.github.com>
2022-02-01 14:57:40 +0100

    Variable fixes

--- scripts/controllers/groupListController.js
+++ scripts/controllers/groupListController.js
@@ -3,16 +3,16 @@
  * @description Adds the groupId to the list of marked groups when they are turned on, and removes them when they are turned off.
  */
 function editMarkedGroups(isChecked, groupId) {
-    const markedGroupIds = model.inputs.groupList.markedGroupIds
+    const groupList = model.inputs.groupList;
     if (isChecked === true) {
-        markedGroupIds.push(groupId);
+        groupList.markedGroupIds.push(groupId);
     }
     if (isChecked === false) {
-        model.inputs.groupList.checkedAll = false;
-        let index = markedGroupIds.findIndex(function (id) {
+        groupList.checkedAll = false;
+        let index = groupList.markedGroupIds.findIndex(function (id) {
             return id === groupId;
         });
-        markedGroupIds.splice(index, 1);
+        groupList.markedGroupIds.splice(index, 1);
     }
     updateMainView();
 }
@@ -21,18 +21,18 @@ function editMarkedGroups(isChecked, groupId) {
  * @description Adds all missing groups to the marked group array
  */
 function checkAllBox(isChecked) {
-    let markedGroupIds = model.inputs.groupList.markedGroupIds;
+    const groupList = model.inputs.groupList;
     const groupIds = getGroupsFromUserID(model.app.userLoggedInId, true);
     if (isChecked === true) {
-        model.inputs.groupList.checkedAll = true;
-        model.inputs.groupList.markedGroupIds = [];
+        groupList.checkedAll = true;
+        groupList.markedGroupIds = [];
         for (const group of groupIds) {
-            model.inputs.groupList.markedGroupIds.push(group.id);
+            groupList.markedGroupIds.push(group.id);
         }
     }
     else if (isChecked === false) {
-        model.inputs.groupList.markedGroupIds = [];
-        model.inputs.groupList.checkedAll = false;
+        groupList.markedGroupIds = [];
+        groupList.checkedAll = false;
     }
     updateMainView();
 }
