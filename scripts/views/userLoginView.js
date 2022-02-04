7e41df2e81be80a280551bf378e95abb911aaf3e
Lillie <75392499+iLillie@users.noreply.github.com>
2022-02-04 14:36:37 +0100

    refactor: GroupNew, UserLogin, SignUp

--- scripts/views/userLoginView.js
+++ scripts/views/userLoginView.js
@@ -1,30 +1,31 @@
 function updateUserLoginView() {
-    return /*html*/`
-    <div>
-    <h1>Login side</h1>
-    <h2>Login to Continue</h2>
-    <form name="login" onsubmit="handleLoginOnClick(); return false">
-      <div class="input-items">
-        <label>Email: </label>
-        ${emailInputHTML(model.inputs.userLogin.email, "model.inputs.userLogin.email = this.value")}
-      </div>
-      <div class="input-items">
-        <label>Password (minimum 8 characters): </label>
-        ${passwordInputHTML(model.inputs.userLogin.password, "model.inputs.userLogin.password = this.value")}
-      </div>
-      <button type="submit">login</button>
-    </form>
-    <button onclick="redirectToPage('UserSignup')">Sign up</button>
-      ${isUserLoginCorrect()}
-    </div>
-    `
+  const userLogin = model.inputs.userLogin;
+    const userLoginViewInputs = {
+        email: {
+          labelText: 'Epost:',
+          value: userLogin.email,
+          onChange: 'model.inputs.userLogin.email = this.value',
+          placeholderText: 'Skriv inn email..',
+          isRequired: true,
+        },
+        password: {
+          labelText: 'Passord:',
+          savePass: false,
+          value: userLogin.password,
+          onChange: 'model.inputs.userLogin.password = this.value',
+          placeholderText: 'Skriv inn passord..'
+        }
+      }
+        
+  return /*html*/`
+       <div>
+       <h1>Logg inn</h1>
+       <form id="login-form" onsubmit="handleLoginOnClick(); return false">
+         ${inputEmailWithLabeHTML(userLoginViewInputs.email)}
+         ${passwordInputWithLabelHTML(userLoginViewInputs.password)}         
+       </form>
+       <button form="login-form" type="submit">Logg inn</button>
+       <button onclick="redirectToPage('UserSignup')">Registrer</button>
+        ${userLogin.isCorrect === false ? '<p style="color: red;">Email or password is wrong</p>' : ''}
+      </div>`
 }
-
-function isUserLoginCorrect(){
-  const isCorrect = model.inputs.userLogin.isCorrect;
-  if (isCorrect === false) {
-    return '<p style="color: red;">Email or password is wrong</p>'    
-  }
-  return ''
-}
-
--- scripts/views/userSignupView.js
+++ scripts/views/userSignupView.js
@@ -1,62 +1,56 @@
 function updateUserSignupView() {
-  let html = '';
-  html += /*html*/`
-  <div>
-  <h1>Sign up</h1>
-  <h2>Sign up to get an account</h2>
-  <form name="login" onsubmit="handleSignupOnClick(); return false">
-    <div class="input-items">
-      <label>First name </label>
-      <input
-        value="${model.inputs.userSignup.firstName}"
-        onchange="model.inputs.userSignup.firstName = this.value"
-        type="text" 
-        placeholder="First name" 
-        required>
-    </div>
-    <div class="input-items">
-      <label>Last name </label>
-      <input 
-       value="${model.inputs.userSignup.lastName}"
-       onchange="model.inputs.userSignup.lastName = this.value"
-       type="text" 
-       placeholder="Last name" 
-       required>
-    </div>
-    <div class="input-items">
-      <label>Email: </label>
-      ${emailInputHTML(model.inputs.userSignup.email, "model.inputs.userSignup.email = this.value")}
-    </div>
-    <div class="input-items">
-      <label>Password (minimum 8 characters): </label>
-      ${passwordInputHTML(model.inputs.userSignup.password, "model.inputs.userSignup.password = this.value")}
-    </div>
-    <div class="input-items">
-      <label>Confirm Password: </label>
-      ${passwordInputHTML(model.inputs.userSignup.confirmPassword, "model.inputs.userSignup.confirmPassword = this.value")}
-    </div>
-    <input type="submit" value="Sign up"/>
-  </form>
-  <button onclick="redirectToPage('UserLogin')">Return to login</button>
-  ${doPasswordsMatch()}
-  ${isEmailUnique()}
-  </div>
-    `
-  return html;
-}
-
-function doPasswordsMatch() {
-  const isCorrect = model.inputs.userSignup.confirmCreation;
-  if (isCorrect === false) {
-    return '<p style="color: red;">Passwords do not match</p>'
+  const userSignup = model.inputs.userSignup;
+  const userSignupViewInputs = {
+    firstName: {
+      labelText: 'Fornavn:',
+      value: userSignup.firstName,
+      onChange: 'model.inputs.userSignup.firstName = this.value',
+      placeholderText: 'Skriv inn fornavn',
+      isRequired: true,
+    },
+    lastName: {
+      labelText: 'Etternavn:',
+      value: userSignup.lastName,
+      onChange: 'model.inputs.userSignup.lastName = this.value',
+      placeholderText: 'Skriv inn etternavn..',
+      isRequired: true,
+    },
+    email: {
+      labelText: 'Epost: ',
+      value: userSignup.email,
+      onChange: 'model.inputs.userSignup.email = this.value',
+      placeholderText: 'Skriv inn epost..',
+      isRequired: true,
+    },
+    password: {
+      labelText: 'Passord:',
+      savePass: false,
+      value: userSignup.password,
+      onChange: 'model.inputs.userSignup.password = this.value',
+      placeholderText: 'Skriv inn passord..'
+    },
+    confirmPassword: {
+      labelText: 'Bekrefte passord:',
+      savePass: false,
+      value: userSignup.confirmPassword,
+      onChange: 'model.inputs.userSignup.confirmPassword = this.value',
+      placeholderText: 'Skriv inn passord igjen..'
+    },
   }
-  return '';
-}
 
-function isEmailUnique() {
-  const isCorrect = model.inputs.userSignup.confirmEmail;
-  if (isCorrect === false) {
-    return '<p style="color: red;">Email has already been used</p>'
-  }
-  return '';
+  return /*html*/`
+       <div>
+       <h1>Registrer bruker</h1>
+       <form id="signup-form" onsubmit="handleSignupOnClick(); return false">
+          ${inputTextWithLabelHTML(userSignupViewInputs.firstName)}
+          ${inputTextWithLabelHTML(userSignupViewInputs.lastName)}
+          ${inputEmailWithLabeHTML(userSignupViewInputs.email)}
+          ${passwordInputWithLabelHTML(userSignupViewInputs.password)}         
+          ${passwordInputWithLabelHTML(userSignupViewInputs.confirmPassword)} 
+       </form>
+       <button form="signup-form" type="submit">Registrer</button>
+       <button onclick="redirectToPage('UserLogin')">Tilbake</button>
+       ${userSignup.confirmCreation === false ? '<p style="color: red;">Passwords do not match</p>' : ''}
+       ${userSignup.confirmEmail === false ? '<p style="color: red;">Email has already been used</p>' : ''}
+      </div>`
 }
