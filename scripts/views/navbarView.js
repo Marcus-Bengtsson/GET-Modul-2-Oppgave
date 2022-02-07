d38e58c678d9ffdaf752fd6f95a296bb2a01f729
Lillie <75392499+iLillie@users.noreply.github.com>
2022-02-07 12:57:43 +0100

    Update components

--- scripts/views/navbarView.js
+++ scripts/views/navbarView.js
@@ -1,12 +1,37 @@
 function navBar() {
+    let iconInputs = {
+        profile: {
+            iconName: 'account_circle'
+        },
+        dashboard: {
+            iconName: 'home',
+            page: 'Dashboard'
+        },
+        notifications: {
+            iconName: 'notifications',
+            className: 'notification-icon'
+        },
+        logout: {
+            onClick: 'logout()',
+            iconName: 'logout',
+        },
+        grouplist: {
+            iconName: 'groups',
+            page: 'GroupList',
+        },
+        survey: {
+          iconName: 'poll',
+          page: 'SurveyPage', 
+        },
+    }
     return /*html*/`
     <nav aria-label="hoved" class="navbar-main">
-        ${iconHTML('account_circle')}
-        ${iconButtonHTML('logout', 'logout()')}
-        ${iconRedirectButtonHTML('home', 'Dashboard')}
-        ${iconHTML('notifications', 'notification-icon')}
-        ${iconRedirectButtonHTML('groups', 'GroupList')}
-        ${iconRedirectButtonHTML('poll', 'SurveyPage')} 
+        ${iconHTML(iconInputs.profile)}
+        ${iconButtonHTML(iconInputs.logout)}
+        ${iconRedirectButtonHTML(iconInputs.dashboard)}
+        ${iconHTML(iconInputs.notifications)}
+        ${iconRedirectButtonHTML(iconInputs.grouplist)}
+        ${iconRedirectButtonHTML(iconInputs.survey)} 
     </nav>
     `
 }
